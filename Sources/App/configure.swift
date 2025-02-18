5113ec9d150d4c0654ec80ec000bb895845c5615
Patrick Lawler <patlaw777@icloud.com>
2025-02-18 22:09:10 +0700

    reconfigured heroku

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -8,12 +8,11 @@ public func configure(_ app: Application) async throws {
     // uncomment to serve files from /Public folder
     // app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
     
-    
     if let databaseURLString = Environment.get("DATABASE_URL"),
        let databaseURL = URL(string: databaseURLString),
        let username = databaseURL.user,
        let password = databaseURL.password {
-       let hostname = databaseURL.host ?? "localhost"
+        let hostname = databaseURL.host ?? "localhost"
         let port = databaseURL.port ?? 5432
         let databaseName = databaseURL.path.trimmingCharacters(in: CharacterSet(charactersIn: "/"))
         var tlsConfig = TLSConfiguration.makeClientConfiguration()
@@ -27,40 +26,39 @@ public func configure(_ app: Application) async throws {
             database: databaseName,
             tls: .require(try .init(configuration: tlsConfig)) // Updated TLS handling
         )
-
+        
         app.databases.use(
             .postgres(
-                configuration: postgresConfig, // Ensure this is passed properly
+                configuration: postgresConfig,
                 maxConnectionsPerEventLoop: 10,
                 connectionPoolTimeout: .seconds(10),
                 encodingContext: .default,
                 decodingContext: .default,
                 sqlLogLevel: .info
-            ),
-            as: .psql
-        )
-
+            ), as: .psql)
     }
     else {
-        app.databases.use(DatabaseConfigurationFactory.postgres(configuration: .init(
-            hostname: Environment.get("DATABASE_HOST") ?? "localhost",
-            port: Environment.get("DATABASE_PORT").flatMap(Int.init(_:)) ?? SQLPostgresConfiguration.ianaPortNumber,
-            username: Environment.get("DATABASE_USERNAME") ?? "vapor_username",
-            password: Environment.get("DATABASE_PASSWORD") ?? "vapor_password",
-            database: Environment.get("DATABASE_NAME") ?? "vapor_database",
-            tls: .prefer(try .init(configuration: .clientDefault)))
-        ), as: .psql)
         
+        let postgresConfig = SQLPostgresConfiguration(
+            hostname: "localhost",
+            port: 5432,
+            username: "vapor_username",
+            password: "vapor_password",
+            database: "vapor_database",
+            tls: .prefer(try .init(configuration: .clientDefault))
+        )
+        
+        app.databases.use(
+            .postgres(configuration: postgresConfig), as: .psql)
     }
     
-
     app.migrations.add(CreateUsers())
     app.migrations.add(CreateEvents())
     app.migrations.add(UpdatingUserModel())
     if app.environment == .development {
         try await app.autoMigrate().get()
     }
-
+    
     // register routes
     try routes(app)
 }
